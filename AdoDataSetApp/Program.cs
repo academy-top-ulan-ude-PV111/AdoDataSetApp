ce023218c2f657d5b55fce0f8c0d490f1205fb2f
maxefimov <efimov.top.academy@gmail.com>
2023-01-15 12:55:12 +0300

    15-01-23

--- AdoDataSetApp/Program.cs
+++ AdoDataSetApp/Program.cs
@@ -7,7 +7,7 @@ namespace AdoDataSetApp
     {
         static void Main(string[] args)
         {
-            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LibraryDb;Integrated Security=True;Connect Timeout=10;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LibraryDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
@@ -15,6 +15,20 @@ namespace AdoDataSetApp
                 SqlDataAdapter adapter = new(sqlQuery, connection);
                 DataSet dataSet = new();
                 adapter.Fill(dataSet);
+
+                foreach(DataTable table in dataSet.Tables)
+                {
+                    foreach(DataColumn column in table.Columns)
+                        Console.Write($"{column.ColumnName}\t");
+                    Console.WriteLine();
+                    foreach(DataRow row in table.Rows)
+                    {
+                        var rowArr = row.ItemArray;
+                        foreach(object item in rowArr)
+                            Console.Write($"{item.ToString()}\t");
+                        Console.WriteLine();
+                    }
+                }
             }
         }
     }
