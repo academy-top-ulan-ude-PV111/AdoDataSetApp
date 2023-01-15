46ec9693e40a4faf479c296c927a3663c696cf58
maxefimov <efimov.academy.top@gmail.com>
2023-01-15 11:49:48 +0300

    Добавьте файлы проекта.

---
+++ AdoDataSetApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace AdoDataSetApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LibraryDb;Integrated Security=True;Connect Timeout=10;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sqlQuery = "SELECT * FROM books";
+                SqlDataAdapter adapter = new(sqlQuery, connection);
+                DataSet dataSet = new();
+                adapter.Fill(dataSet);
+            }
+        }
+    }
+}
